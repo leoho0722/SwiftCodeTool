44d6c0004544b84196862ed81995e4302d6e0350
leoho0722 <leo160918@gmail.com>
2022-11-05 14:44:31 +0800

    新增 Controller Previews.xctemplate，讓 UIKit 也可以使用 SwiftUI Previews 功能

---
+++ Xcode Template/Controller Previews.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,18 @@
+//___FILEHEADER___
+
+import SwiftUI
+
+struct ___FILEBASENAMEASIDENTIFIER___: UIViewControllerRepresentable, PreviewProvider {
+
+    func makeUIViewController(context: Context) -> ___VARIABLE_Controller___ {
+        return ___VARIABLE_Controller___()
+    }
+    
+    func updateUIViewController(_ uiViewController: ___VARIABLE_Controller___, context: Context) {
+        
+    }
+    
+    static var previews: some View {
+        ___FILEBASENAMEASIDENTIFIER___()
+    }
+}
